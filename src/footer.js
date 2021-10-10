36ad137680c2c9a3d23b658787b329abfe314089
Faruq <faruqabdulsalam@yahoo.com>
2021-10-10 22:43:43 +0100

    Add footer file

---
+++ src/footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Icon1 from './images/reactComponent/footer/Home'
+import Icon2 from './images/reactComponent/footer/Buy'
+import Icon3 from './images/reactComponent/footer/Deals'
+import Icon4 from './images/reactComponent/footer/Wallet'
+import Icon5 from './images/reactComponent/footer/More'
+
+
+const footer = [
+    {
+      id: 1,
+      icon: <Icon1 />,
+      name: "Home",
+    },
+    {
+      id: 2,
+      icon: <Icon2 />,
+      name: "Buy",
+    },
+    {
+      id: 3,
+      icon: <Icon3 />,
+      name: "Deals",
+    },
+    {
+      id: 4,
+      icon: <Icon4 />,
+      name: "Wallet",
+    },
+    {
+      id: 5,
+      icon: <Icon5 />,
+      name: "More",
+    }
+  ];
+  
+  export default footer
