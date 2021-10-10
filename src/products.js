6dca58c045353f75d740cf9c6ed0fccb6d895f52
Faruq <faruqabdulsalam@yahoo.com>
2021-10-10 22:44:04 +0100

    Add products.js file

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import Img1 from './images/svg/img1.svg'
+import Img2 from './images/svg/img2.svg'
+import Img3 from "./images/svg/img3.svg"
+import Img4 from "./images/svg/img4.svg"
+import Img5 from "./images/svg/img5.svg"
+import Img6 from "./images/svg/img6.svg"
+
+const products = [
+    {
+      id: 1,
+      img:Img1,
+      name: "Free sample small tote bag gucci fen...",
+      lprice:90,
+      hprice:120,
+      piece: "MOQ 4(pieces)"
+    },
+    {
+      id: 2,
+      img: Img2,
+      name: "Free sample small tote bag gucci fen...",
+      lprice:1200,
+      hprice:4000,
+      piece: "MOQ 4(pieces)"
+    },
+    {
+      id: 3,
+      img: Img3,
+      name: "Free sample small gucci fendi dior",
+      lprice:900,
+      hprice:1200,
+      piece: "MOQ 4(pieces)"
+    },
+    {
+      id: 4,
+      img: Img4,
+      name: "Iphone 12",
+      lprice:90,
+      hprice:1200,
+      piece: "MOQ 4(pieces)"
+    },
+    {
+      id: 5,
+      img: Img5,
+      name: "Ipad Mini",
+      lprice:300,
+      hprice:1200,
+      piece: "MOQ 4(pieces)"
+    },
+    {
+      id: 6,
+      img: Img6,
+      name: "Magic Mouse",
+      lprice:350,
+      hprice:700,
+      piece: "MOQ 4(pieces)"
+    }
+  ];
+  
+  export default products
