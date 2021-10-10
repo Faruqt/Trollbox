5c2017d78c413f5347f3bfecf913e7071f114e60
Faruq <faruqabdulsalam@yahoo.com>
2021-10-10 22:44:28 +0100

    Add smallcards.js

---
+++ src/smallcards.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Icon1 from './images/reactComponent/Icon1'
+import Icon2 from './images/reactComponent/Icon2'
+import Icon3 from './images/reactComponent/Icon3'
+import Icon4 from './images/reactComponent/Icon4'
+
+const smallcard = [
+    {
+      id: 1,
+      img: <Icon1 />,
+      name: "Product categories",
+    },
+    {
+      id: 2,
+      img: <Icon2 />,
+      name: "Popular Products",
+    },
+    {
+      id: 3,
+      img: <Icon3 />,
+      name: "Recommended Products",
+    },
+    {
+      id: 4,
+      img: <Icon4 />,
+      name: "Shops",
+    }
+  ];
+  
+  export default smallcard
